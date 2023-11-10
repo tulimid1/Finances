2bb971ec906544751efa8a3a7aaca323f480ae1a
Duncan Tulimieri <tulimid@udel.edu>
2023-11-10 15:00:22 -0500

    initialize production code

--- Investments/Investor.py
+++ Investments/Investor.py
@@ -1,3 +1,109 @@
 class Investor(object):
-    def __init__(self) -> None:
-        pass
+    RETIREMENT_AGE = 65
+    portfolio_amount = []
+
+    def __init__(
+        self,
+        current_age=26,
+        investment_per_month=500,
+        percent_rate_of_return=8,
+        n_years_investing=0,
+        last_name="",
+        first_name="",
+    ):
+        self.CURRENT_AGE = current_age
+        self.INVESTMENT_PER_MONTH = investment_per_month
+        self.PERCENT_RATE_OF_RETURN = percent_rate_of_return
+        self.N_YEARS_INVESTING = n_years_investing
+        self.LAST_NAME = last_name
+        self.FIRST_NAME = first_name
+
+    def calculate_years_to_retirement(self):
+        return self.RETIREMENT_AGE - self.CURRENT_AGE
+
+    def calculate_portfolio_amount(self):
+        n_months_per_year = 12
+        monthly_rate_of_return = self.PERCENT_RATE_OF_RETURN / 100 / n_months_per_year
+        for i_year in range(self.calculate_years_to_retirement()):
+            for j_month in range(n_months_per_year):
+                is_going_to_invest = i_year <= self.N_YEARS_INVESTING
+                if is_going_to_invest:
+                    investment_amount = self.INVESTMENT_PER_MONTH
+                else:
+                    investment_amount = 0
+
+                is_first_year_first_month = i_year == 0 and j_month == 0
+                if is_first_year_first_month:
+                    if is_going_to_invest:
+                        j_portfolio_amount = investment_amount
+                    else:
+                        j_portfolio_amount = 0
+                else:
+                    j_portfolio_amount += j_portfolio_amount * monthly_rate_of_return
+                    j_portfolio_amount += investment_amount
+
+            self.portfolio_amount.append(j_portfolio_amount)
+
+    @property
+    def FIRST_NAME(self):
+        return self._FIRST_NAME
+
+    @FIRST_NAME.setter
+    def FIRST_NAME(self, name=""):
+        if type(name) is not str:
+            raise TypeError("first_name must be a string.\n")
+        self._FIRST_NAME = name
+
+    @property
+    def LAST_NAME(self):
+        return self._LAST_NAME
+
+    @LAST_NAME.setter
+    def LAST_NAME(self, name=""):
+        if type(name) is not str:
+            raise TypeError("last_name must be a string.\n")
+        self._LAST_NAME = name
+
+    @property
+    def N_YEARS_INVESTING(self):
+        return self._N_YEARS_INVESTING
+
+    @N_YEARS_INVESTING.setter
+    def N_YEARS_INVESTING(self, n_years=0):
+        is_time_traveling = n_years < 0
+        if is_time_traveling:
+            raise ValueError("n_years_investing must be greater than 0.\n")
+        self._N_YEARS_INVESTING = n_years
+
+    @property
+    def PERCENT_RATE_OF_RETURN(self):
+        return self._PERCENT_RATE_OF_RETURN
+
+    @PERCENT_RATE_OF_RETURN.setter
+    def PERCENT_RATE_OF_RETURN(self, percent=8):
+        rate_too_low = percent < 0
+        if rate_too_low:
+            raise ValueError("percent_rate_of_return must be greater than 0.\n")
+        self._PERCENT_RATE_OF_RETURN = percent
+
+    @property
+    def INVESTMENT_PER_MONTH(self):
+        return self._INVESTMENT_PER_MONTH
+
+    @INVESTMENT_PER_MONTH.setter
+    def INVESTMENT_PER_MONTH(self, investment_amount=0):
+        is_taking_from_investment = investment_amount < 0
+        if is_taking_from_investment:
+            raise ValueError("investment_per_month must be positive.\n")
+        self._INVESTMENT_PER_MONTH = investment_amount
+
+    @property
+    def CURRENT_AGE(self):
+        return self._CURRENT_AGE
+
+    @CURRENT_AGE.setter
+    def CURRENT_AGE(self, age=26):
+        has_not_been_born = age < 1
+        if has_not_been_born:
+            raise ValueError("current_age must be >= 1.\n")
+        self._CURRENT_AGE = age
--- Investments/Investor_test.py
+++ Investments/Investor_test.py
@@ -5,7 +5,7 @@ from Investor import Investor
 
 class Investor_Test(unittest.TestCase):
     example_investor = Investor()
-    
+
     def test_portfolio_amount_case_2(self):
         dan = Investor(
             current_age=45,
@@ -14,7 +14,7 @@ class Investor_Test(unittest.TestCase):
             n_years_investing=20,
         )
         dan.calculate_portfolio_amount()
-        self.assertEqual(dan.portfolio_amount[-1], 118589)
+        self.assertEqual(round(dan.portfolio_amount[-1]), 117804)
 
     def test_portfolio_amount_case_1(self):
         sally = Investor(
@@ -24,7 +24,7 @@ class Investor_Test(unittest.TestCase):
             n_years_investing=10,
         )
         sally.calculate_portfolio_amount()
-        self.assertEqual(sally.portfolio_amount[-1], 181469)
+        self.assertEqual(round(sally.portfolio_amount[-1]), 191595)
 
     def test_investor_has_calculate_portfolio_amount(self):
         self.assertTrue("calculate_portfolio_amount" in dir(self.example_investor))
@@ -45,10 +45,10 @@ class Investor_Test(unittest.TestCase):
             Investor(percent_rate_of_return="not float")
 
     def test_investor_has_percent_rate_of_return(self):
-        self.assertTrue("percent_rate_of_return" in dir(self.example_investor))
+        self.assertTrue("PERCENT_RATE_OF_RETURN" in dir(self.example_investor))
 
     def test_calculate_years_to_retirement_case_1(self):
-        me = Investor(age=35)
+        me = Investor(current_age=35)
         self.assertEqual(me.calculate_years_to_retirement(), 30)
 
     def test_investor_has_calculate_years_to_retirement(self):
@@ -69,7 +69,7 @@ class Investor_Test(unittest.TestCase):
             Investor(n_years_investing="not an int")
 
     def test_investor_has_n_year_investing(self):
-        self.assertTrue("n_years_investing" in dir(self.example_investor))
+        self.assertTrue("N_YEARS_INVESTING" in dir(self.example_investor))
 
     def test_investment_per_month_error_not_positive(self):
         with self.assertRaises(ValueError):
@@ -80,7 +80,7 @@ class Investor_Test(unittest.TestCase):
             Investor(investment_per_month="not an int")
 
     def test_investor_has_investment_per_month(self):
-        self.assertTrue("investment_per_month" in dir(self.example_investor))
+        self.assertTrue("INVESTMENT_PER_MONTH" in dir(self.example_investor))
 
     def test_current_age_error_not_positive(self):
         with self.assertRaises(ValueError):
@@ -91,31 +91,31 @@ class Investor_Test(unittest.TestCase):
             Investor(current_age="not an int")
 
     def test_investor_has_current_age(self):
-        self.assertTrue("current_age" in dir(self.example_investor))
+        self.assertTrue("CURRENT_AGE" in dir(self.example_investor))
 
     def test_last_name_set_property(self):
-        last_name = "Tulimieri"
+        last_name = "TEST"
         me = Investor(last_name=last_name)
-        self.assertEqual(me.last_name, last_name)
+        self.assertEqual(me.LAST_NAME, last_name)
 
     def test_error_last_name_not_str(self):
         with self.assertRaises(TypeError):
             Investor(last_name=1)
 
     def test_investor_has_last_name(self):
-        self.assertTrue("last_name" in dir(self.example_investor))
+        self.assertTrue("LAST_NAME" in dir(self.example_investor))
 
     def test_first_name_set_property(self):
-        first_name = "Duncan"
+        first_name = "TEST"
         me = Investor(first_name=first_name)
-        self.assertEqual(me.first_name, first_name)
+        self.assertEqual(me.FIRST_NAME, first_name)
 
     def test_error_first_name_not_str(self):
         with self.assertRaises(TypeError):
             Investor(first_name=1)
 
     def test_investor_has_first_name(self):
-        self.assertTrue("first_name" in dir(self.example_investor))
+        self.assertTrue("FIRST_NAME" in dir(self.example_investor))
 
     def test_investor_is_class(self):
         self.assertTrue(os.path.isfile("Investor.py"))
